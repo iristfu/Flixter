9259d312754e4996ed3dcfffdd87d2b8c2963c32
Iris Fu <irisfu@fb.com>
2022-06-15 13:14:43 -0700

    duplicate commit

--- Flixter/MovieViewController.m
+++ Flixter/MovieViewController.m
@@ -8,14 +8,41 @@
 #import "MovieViewController.h"
 
 @interface MovieViewController ()
+@property (nonatomic, strong) NSArray *movies;
+  //remember when a property is set, you must call it with self.myArray
 
 @end
 
 @implementation MovieViewController
 
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    NSURL *url = [NSURL URLWithString:@"https://api.themoviedb.org/3/movie/now_playing?api_key=6eae1b2c2fcc45bb59980b5fb824e222"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:10.0];
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
+    NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+           if (error != nil) {
+               NSLog(@"%@", [error localizedDescription]);
+           }
+           else {
+               NSDictionary *dataDictionary = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
+//               NSLog(@"%@", dataDictionary);// log an object with the %@ formatter.
+               
+               // TODO: Get the array of movies
+               self.movies = dataDictionary[@"results"];
+               for (NSString* movie in self.movies) {
+                   NSLog(@"%@", movie);
+               }
+               // TODO: Store the movies in a property to use elsewhere
+               // TODO: Reload your table view data
+               
+           }
+       }];
+    [task resume];
+    
 }
 
 /*
